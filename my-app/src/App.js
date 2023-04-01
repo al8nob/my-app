b57f50572fd1eb7ac895530583aa44481de8bf11
ahmedZaalani <a7mdxkiller@gmail.com>
2023-04-01 13:37:38 +0100

    adding meta tags and OGP data

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,18 @@
 import "./App.css";
+import Header from "./Header";
+import Nav from "./Nav";
+import Main from "./Main";
+import Footer from "./Footer";
 
 function App() {
-  return <div>Homepage</div>;
+  return (
+    <>
+      <Header />
+      <Nav />
+      <Main />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
